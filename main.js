5c88d1704b70393a42a35628cd0710f0da581cce
YichengViper <99467148+YichengViper@users.noreply.github.com>
2022-02-11 17:20:35 +0800

    Update main.js

--- main.js
+++ main.js
@@ -119,11 +119,11 @@ function aboutMe(){
     if(i==0){
         i=1;
         slogan.style.height="100px";
-        slogan2.innerHTML="这里是高坂滑稽果，一个果厨~ 这个废柴能熟练掌握面向百度编程、Windows和Linux的开关机、PS和PR的安装和卸载、Arduino和各种零件的购买和损坏，还喜欢打音游（（（。。。生命不息，折腾不止！目前坐等高考中...";
+        slogan2.innerHTML="这里Yicheng，网名Viper，我会各种汇编语言,反汇编,虚幻引擎,Unity引擎,c语言,java,Python(只会msgbox的屑),易语言（（（。。。生命不息，折腾不止！目前坐等小升初...";
     } else {
         i=0;
         slogan.style.height="50px";
-        slogan2.innerHTML="如果奇迹有颜色，那么一定是橙色！";
+        slogan2.innerHTML="12岁,嘉心糖";
     }
     
 }
